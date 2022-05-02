56f96bd2df42d826adb6be0f1283618cbf195fa6
Parsa33033 <parsa_h_m@outlook.com>
2022-05-02 18:28:40 +0200

    .

---
+++ candlestick-builder/target/generated-sources/annotations/com/tr/candlestickbuilder/model/mapper/CandlestickDocumentMapperImpl.java
@@ -0,0 +1,128 @@
+package com.tr.candlestickbuilder.model.mapper;
+
+import com.tr.candlestickbuilder.model.dto.CandlestickDTO;
+import com.tr.candlestickbuilder.model.mongodb.CandlestickDocument;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2022-05-02T08:08:01+0200",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.2 (Oracle Corporation)"
+)
+@Component
+public class CandlestickDocumentMapperImpl implements CandlestickDocumentMapper {
+
+    @Override
+    public CandlestickDTO toDto(CandlestickDocument entity) {
+        if ( entity == null ) {
+            return null;
+        }
+
+        CandlestickDTO candlestickDTO = new CandlestickDTO();
+
+        candlestickDTO.setIsin( entity.getIsin() );
+        if ( entity.getOpenTimestamp() != null ) {
+            candlestickDTO.setOpenTimestamp( entity.getOpenTimestamp().toString() );
+        }
+        candlestickDTO.setOpenPrice( entity.getOpenPrice() );
+        candlestickDTO.setHighPrice( entity.getHighPrice() );
+        candlestickDTO.setLowPrice( entity.getLowPrice() );
+        candlestickDTO.setClosingPrice( entity.getClosingPrice() );
+        if ( entity.getCloseTimestamp() != null ) {
+            candlestickDTO.setCloseTimestamp( entity.getCloseTimestamp().toString() );
+        }
+
+        return candlestickDTO;
+    }
+
+    @Override
+    public CandlestickDocument toEntity(CandlestickDTO dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        CandlestickDocument candlestickDocument = new CandlestickDocument();
+
+        if ( dto.getOpenTimestamp() != null ) {
+            candlestickDocument.setOpenTimestamp( Instant.parse( dto.getOpenTimestamp() ) );
+        }
+        candlestickDocument.setOpenPrice( dto.getOpenPrice() );
+        candlestickDocument.setHighPrice( dto.getHighPrice() );
+        candlestickDocument.setLowPrice( dto.getLowPrice() );
+        candlestickDocument.setClosingPrice( dto.getClosingPrice() );
+        if ( dto.getCloseTimestamp() != null ) {
+            candlestickDocument.setCloseTimestamp( Instant.parse( dto.getCloseTimestamp() ) );
+        }
+        candlestickDocument.setIsin( dto.getIsin() );
+
+        return candlestickDocument;
+    }
+
+    @Override
+    public List<CandlestickDTO> toDtoLists(List<CandlestickDocument> entities) {
+        if ( entities == null ) {
+            return null;
+        }
+
+        List<CandlestickDTO> list = new ArrayList<CandlestickDTO>( entities.size() );
+        for ( CandlestickDocument candlestickDocument : entities ) {
+            list.add( toDto( candlestickDocument ) );
+        }
+
+        return list;
+    }
+
+    @Override
+    public List<CandlestickDocument> toEntityLists(List<CandlestickDTO> dtos) {
+        if ( dtos == null ) {
+            return null;
+        }
+
+        List<CandlestickDocument> list = new ArrayList<CandlestickDocument>( dtos.size() );
+        for ( CandlestickDTO candlestickDTO : dtos ) {
+            list.add( toEntity( candlestickDTO ) );
+        }
+
+        return list;
+    }
+
+    @Override
+    public Map<String, CandlestickDTO> toDtoMaps(Map<String, CandlestickDocument> entities) {
+        if ( entities == null ) {
+            return null;
+        }
+
+        Map<String, CandlestickDTO> map = new HashMap<String, CandlestickDTO>( Math.max( (int) ( entities.size() / .75f ) + 1, 16 ) );
+
+        for ( java.util.Map.Entry<String, CandlestickDocument> entry : entities.entrySet() ) {
+            String key = entry.getKey();
+            CandlestickDTO value = toDto( entry.getValue() );
+            map.put( key, value );
+        }
+
+        return map;
+    }
+
+    @Override
+    public Map<String, CandlestickDocument> toEntityMaps(Map<String, CandlestickDTO> dtos) {
+        if ( dtos == null ) {
+            return null;
+        }
+
+        Map<String, CandlestickDocument> map = new HashMap<String, CandlestickDocument>( Math.max( (int) ( dtos.size() / .75f ) + 1, 16 ) );
+
+        for ( java.util.Map.Entry<String, CandlestickDTO> entry : dtos.entrySet() ) {
+            String key = entry.getKey();
+            CandlestickDocument value = toEntity( entry.getValue() );
+            map.put( key, value );
+        }
+
+        return map;
+    }
+}
---
+++ candlestick-builder/target/generated-sources/annotations/com/tr/candlestickbuilder/model/mapper/InstrumentHashMapperImpl.java
@@ -0,0 +1,194 @@
+package com.tr.candlestickbuilder.model.mapper;
+
+import com.tr.candlestickbuilder.model.dto.CandlestickDTO;
+import com.tr.candlestickbuilder.model.dto.InstrumentDTO;
+import com.tr.candlestickbuilder.model.redis.CandlestickHash;
+import com.tr.candlestickbuilder.model.redis.InstrumentHash;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2022-05-02T08:08:01+0200",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.2 (Oracle Corporation)"
+)
+@Component
+public class InstrumentHashMapperImpl implements InstrumentHashMapper {
+
+    @Override
+    public InstrumentDTO toDto(InstrumentHash entity) {
+        if ( entity == null ) {
+            return null;
+        }
+
+        InstrumentDTO instrumentDTO = new InstrumentDTO();
+
+        instrumentDTO.setDescription( entity.getDescription() );
+        instrumentDTO.setIsin( entity.getIsin() );
+        instrumentDTO.setType( entity.getType() );
+        instrumentDTO.setCandlesticks( stringCandlestickHashMapToStringCandlestickDTOMap( entity.getCandlesticks() ) );
+        instrumentDTO.setTimestamp( entity.getTimestamp() );
+
+        return instrumentDTO;
+    }
+
+    @Override
+    public InstrumentHash toEntity(InstrumentDTO dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        InstrumentHash instrumentHash = new InstrumentHash();
+
+        instrumentHash.setDescription( dto.getDescription() );
+        instrumentHash.setType( dto.getType() );
+        instrumentHash.setTimestamp( dto.getTimestamp() );
+        instrumentHash.setIsin( dto.getIsin() );
+        instrumentHash.setCandlesticks( stringCandlestickDTOMapToStringCandlestickHashMap( dto.getCandlesticks() ) );
+
+        return instrumentHash;
+    }
+
+    @Override
+    public List<InstrumentDTO> toDtoLists(List<InstrumentHash> entities) {
+        if ( entities == null ) {
+            return null;
+        }
+
+        List<InstrumentDTO> list = new ArrayList<InstrumentDTO>( entities.size() );
+        for ( InstrumentHash instrumentHash : entities ) {
+            list.add( toDto( instrumentHash ) );
+        }
+
+        return list;
+    }
+
+    @Override
+    public List<InstrumentHash> toEntityLists(List<InstrumentDTO> dtos) {
+        if ( dtos == null ) {
+            return null;
+        }
+
+        List<InstrumentHash> list = new ArrayList<InstrumentHash>( dtos.size() );
+        for ( InstrumentDTO instrumentDTO : dtos ) {
+            list.add( toEntity( instrumentDTO ) );
+        }
+
+        return list;
+    }
+
+    @Override
+    public Map<String, InstrumentDTO> toDtoMaps(Map<String, InstrumentHash> entities) {
+        if ( entities == null ) {
+            return null;
+        }
+
+        Map<String, InstrumentDTO> map = new HashMap<String, InstrumentDTO>( Math.max( (int) ( entities.size() / .75f ) + 1, 16 ) );
+
+        for ( java.util.Map.Entry<String, InstrumentHash> entry : entities.entrySet() ) {
+            String key = entry.getKey();
+            InstrumentDTO value = toDto( entry.getValue() );
+            map.put( key, value );
+        }
+
+        return map;
+    }
+
+    @Override
+    public Map<String, InstrumentHash> toEntityMaps(Map<String, InstrumentDTO> dtos) {
+        if ( dtos == null ) {
+            return null;
+        }
+
+        Map<String, InstrumentHash> map = new HashMap<String, InstrumentHash>( Math.max( (int) ( dtos.size() / .75f ) + 1, 16 ) );
+
+        for ( java.util.Map.Entry<String, InstrumentDTO> entry : dtos.entrySet() ) {
+            String key = entry.getKey();
+            InstrumentHash value = toEntity( entry.getValue() );
+            map.put( key, value );
+        }
+
+        return map;
+    }
+
+    protected CandlestickDTO candlestickHashToCandlestickDTO(CandlestickHash candlestickHash) {
+        if ( candlestickHash == null ) {
+            return null;
+        }
+
+        CandlestickDTO candlestickDTO = new CandlestickDTO();
+
+        candlestickDTO.setIsin( candlestickHash.getIsin() );
+        if ( candlestickHash.getOpenTimestamp() != null ) {
+            candlestickDTO.setOpenTimestamp( candlestickHash.getOpenTimestamp().toString() );
+        }
+        candlestickDTO.setOpenPrice( candlestickHash.getOpenPrice() );
+        candlestickDTO.setHighPrice( candlestickHash.getHighPrice() );
+        candlestickDTO.setLowPrice( candlestickHash.getLowPrice() );
+        candlestickDTO.setClosingPrice( candlestickHash.getClosingPrice() );
+        if ( candlestickHash.getCloseTimestamp() != null ) {
+            candlestickDTO.setCloseTimestamp( candlestickHash.getCloseTimestamp().toString() );
+        }
+
+        return candlestickDTO;
+    }
+
+    protected Map<String, CandlestickDTO> stringCandlestickHashMapToStringCandlestickDTOMap(Map<String, CandlestickHash> map) {
+        if ( map == null ) {
+            return null;
+        }
+
+        Map<String, CandlestickDTO> map1 = new HashMap<String, CandlestickDTO>( Math.max( (int) ( map.size() / .75f ) + 1, 16 ) );
+
+        for ( java.util.Map.Entry<String, CandlestickHash> entry : map.entrySet() ) {
+            String key = entry.getKey();
+            CandlestickDTO value = candlestickHashToCandlestickDTO( entry.getValue() );
+            map1.put( key, value );
+        }
+
+        return map1;
+    }
+
+    protected CandlestickHash candlestickDTOToCandlestickHash(CandlestickDTO candlestickDTO) {
+        if ( candlestickDTO == null ) {
+            return null;
+        }
+
+        CandlestickHash candlestickHash = new CandlestickHash();
+
+        if ( candlestickDTO.getOpenTimestamp() != null ) {
+            candlestickHash.setOpenTimestamp( Instant.parse( candlestickDTO.getOpenTimestamp() ) );
+        }
+        candlestickHash.setOpenPrice( candlestickDTO.getOpenPrice() );
+        candlestickHash.setHighPrice( candlestickDTO.getHighPrice() );
+        candlestickHash.setLowPrice( candlestickDTO.getLowPrice() );
+        candlestickHash.setClosingPrice( candlestickDTO.getClosingPrice() );
+        if ( candlestickDTO.getCloseTimestamp() != null ) {
+            candlestickHash.setCloseTimestamp( Instant.parse( candlestickDTO.getCloseTimestamp() ) );
+        }
+        candlestickHash.setIsin( candlestickDTO.getIsin() );
+
+        return candlestickHash;
+    }
+
+    protected Map<String, CandlestickHash> stringCandlestickDTOMapToStringCandlestickHashMap(Map<String, CandlestickDTO> map) {
+        if ( map == null ) {
+            return null;
+        }
+
+        Map<String, CandlestickHash> map1 = new HashMap<String, CandlestickHash>( Math.max( (int) ( map.size() / .75f ) + 1, 16 ) );
+
+        for ( java.util.Map.Entry<String, CandlestickDTO> entry : map.entrySet() ) {
+            String key = entry.getKey();
+            CandlestickHash value = candlestickDTOToCandlestickHash( entry.getValue() );
+            map1.put( key, value );
+        }
+
+        return map1;
+    }
+}
